6bf3f92f74d2db29c745cf5ca23ceddf74fed8fc
shanshan <794139660@qq.com>
2018-06-09 16:19:31 +0800

    Update picture_remove_noise.py

--- picture_remove_noise.py
+++ picture_remove_noise.py
@@ -3,7 +3,7 @@
 实现图片去除噪音的功能
 思路：
 色素值越大表示颜色越深，越小表示颜色越浅
-变量每个像素点，当色素值>n,把该点画成黑色点，当色素值<n 把该点画成百色点
+遍历每个像素点，当色素值>n,把该点画成黑色点，当色素值<n 把该点画成百色点
 '''
 
 from PIL import Image
@@ -13,7 +13,7 @@ def two_value():
     #灰度图
     lim=image.convert('L')
     # 灰度阈值设为165，低于这个值的点全部填为白色
-    threshold=160
+    threshold=165
     table=[]
     for j in range(256):
         if j<threshold:
